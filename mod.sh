cef22f48d9f4d54f220299a88e233fc2d00afaea
sco1314 <2421462516@qq.com>
2023-03-21 16:53:41 +0800

    更新网站

---
+++ mod.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+an="$GIT_AUTHOR_NAME"
+am="$GIT_AUTHOR_EMAIL"
+cn="$GIT_COMMITTER_NAME"
+cm="$GIT_COMMITTER_EMAIL"
+
+if [ "$GIT_COMMITTER_EMAIL" = "rolitter@gmail.com" ]
+then
+    cn="sco1314"
+    cm="2421462516@qq.com"
+fi
+
+export GIT_AUTHOR_NAME="$an"
+export GIT_AUTHOR_EMAIL="$am"
+export GIT_COMMITTER_NAME="$cn"
+export GIT_COMMITTER_EMAIL="$cm"
+
+' HEAD
+
